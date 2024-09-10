473a63de8305f52aea910215d4a20bb140af0f16
Matt Knop <mknop@redhat.com>
2024-09-10 15:21:48 -0600

    minor cleanup

--- plugins/security-backend/src/service/common/getGrypeGitRepoBranchData.ts
+++ plugins/security-backend/src/service/common/getGrypeGitRepoBranchData.ts
@@ -3,69 +3,62 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import yauzl from 'yauzl';
 
-export const QueryGithubActionsRunsData = (backendUrl: string) => {
+export const QueryGithubActionsRunsData = async (backendUrl: string) => {
 
     // Backstage's proxy does not support redirect urls, therefore we are 
     // manually implementing the redirect
-    const getRedirectUrl = async () => {
-        const url = `${backendUrl}/api/proxy/actions/repos/RedHatInsights/clowder/actions/artifacts/1887237108/zip`;
-        let location: string = "";
+    const url = `${backendUrl}/api/proxy/actions/repos/RedHatInsights/clowder/actions/artifacts/1887237108/zip`;
+    let location: string = "";
+    let fileData = '';
 
-        const headers = {
-          Accept: "application/vnd.github+json",
-          "X-GitHub-Api-Version": "2022-11-28"
-        }
+    const headers = {
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
 
-        const resp = await fetch(url, { method: "get", redirect: "manual",  headers: headers})
-          .then(response => {
-            location = JSON.stringify(response.headers.get("location"));
-          })
-          .catch((_error) => {
-              console.error(`Error fetching location header information`);
-          })
+    const resp = await fetch(url, { method: "get", redirect: "manual",  headers: headers})
+      .then(response => {
+        location = JSON.stringify(response.headers.get("location"));
+      })
+      .catch((_error) => {
+          console.error(`Error fetching location header information`);
+      })
 
-        console.log(location)
+    console.log(location)
 
-        // Download the ZIP file into memory
-        const response = await axios({
-          method: 'get',
-          url: location.slice(1, -1),
-          responseType: 'arraybuffer'
-        });
+    // Download the ZIP file into memory
+    const response = await axios({
+      method: 'get',
+      url: location.slice(1, -1),
+      responseType: 'arraybuffer'
+    });
 
-        const buffer = Buffer.from(response.data);
-        console.log(buffer)
+    const buffer = Buffer.from(response.data);
 
-        // Open the ZIP file from memory
-        yauzl.fromBuffer(buffer, { lazyEntries: true }, (err: any, zipFile: any) => {
-          if (err) throw err;
+    // Open the ZIP file from memory
+    yauzl.fromBuffer(buffer, { lazyEntries: true }, (err: any, zipFile: any) => {
+      if (err) throw err;
 
-          console.log(zipFile)
+      zipFile.readEntry();
 
-          zipFile.readEntry();
+      zipFile.on('entry', (entry: any) => {
+        // if (entry.fileName === "grype-vuln-artifacts-Dockerfile.zip") {
+          // Found the file we want to extract
+          zipFile.openReadStream(entry, (error: any, readStream: any) => {
+            if (error) throw err;
 
-          zipFile.on('entry', (entry: any) => {
-            // if (entry.fileName === "grype-vuln-artifacts-Dockerfile.zip") {
-              // Found the file we want to extract
-              zipFile.openReadStream(entry, (error: any, readStream: any) => {
-                if (error) throw err;
+            // Process the extracted file here
+            readStream.on('data', (chunk: any) => {
+              fileData += chunk;
+            });
 
-                // Process the extracted file here
-                let fileData = '';
-                readStream.on('data', (chunk: any) => {
-                  fileData += chunk;
-                });
-
-                readStream.on('end', () => {
-                  console.log(`Extracted file content: ${fileData}`);
-                  zipFile.close();
-                });
-              });
+            readStream.on('end', () => {
+              console.log(`Extracted file content: ${fileData}`);
+              zipFile.close();
+            });
           });
-        })
-      }
+      });
+    })
 
-    const result = getRedirectUrl()
-
-    return result
+    return fileData
 }
--- plugins/security-backend/src/service/routes/grype.router.ts
+++ plugins/security-backend/src/service/routes/grype.router.ts
@@ -10,25 +10,25 @@ const config = useApi(configApiRef);
 const backendUrl = config.getString('backend.baseUrl');
 
 grype.get('/', async (req, response) => {
-    async function downloadAndExtractFile(url: any, fileNameToExtract: any) {
-        try {
-          // Download the ZIP file into memory
-          const response = await axios({
-            method: 'get',
-            url: url,
-            responseType: 'arraybuffer'
-          });
+    // async function downloadAndExtractFile(url: any, fileNameToExtract: any) {
+    //     try {
+    //       // Download the ZIP file into memory
+    //       const response = await axios({
+    //         method: 'get',
+    //         url: url,
+    //         responseType: 'arraybuffer'
+    //       });
       
-          const buffer = Buffer.from(response.data);
-        } catch (error) {
-          console.error('Error downloading or extracting file:', error);
-        }
-      }
+    //       const buffer = Buffer.from(response.data);
+    //     } catch (error) {
+    //       console.error('Error downloading or extracting file:', error);
+    //     }
+    //   }
       
-      // Example usage:
-      const zipUrl = `${backendUrl}/api/proxy/grype/repos/RedHatInsights/clowder/actions/artifacts`;
-      const fileName = 'output.zip';
-      downloadAndExtractFile(zipUrl, fileName);
+    //   // Example usage:
+    //   const zipUrl = `${backendUrl}/api/proxy/grype/repos/RedHatInsights/clowder/actions/artifacts`;
+    //   const fileName = 'output.zip';
+    //   downloadAndExtractFile(zipUrl, fileName);
 
     response.json({ status: 'ok' });
 });
